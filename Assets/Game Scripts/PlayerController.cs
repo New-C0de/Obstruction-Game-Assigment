d29200eb26b2472096a90ec5d71558bf7bade1f4
GlennFrancis <125676530+New-C0de@users.noreply.github.com>
2026-09-08 20:55:59 -0400

    Systems Update

---
+++ Assets/Game Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 7f;
+
+    private Rigidbody rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
